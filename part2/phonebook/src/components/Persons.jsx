9d21e5a82f582793a24c20c9ff4be2494ebb3783
ecaleb97 <valley_rob.0c@icloud.com>
2023-11-30 20:17:59 +0100

    Refactor Persons component to use Person component

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,27 +1,18 @@
-import { DeleteIcon } from './Icons'
+import { Person } from "./Person"
 
 export function Persons ({ persons, handleDelete }) {
   return (
-    <ul>
+    <ul className="list-disc pl-4 flex flex-col gap-4">
       {
         persons?.map(person => {
           const { id, name, number } = person
           return (
-            <li 
+            <Person
               key={id}
-              className='py-2'
-            >
-              <div className='flex gap-4 items-center'>
-                <span>{name} {number}</span>
-                <button 
-                  className='border border-red-500 text-red-500 rounded-md p-1'
-                  onClick={() => handleDelete(id)}
-                >
-                  <DeleteIcon />
-                </button>
-              </div>
-              
-            </li>
+              name={name}
+              number={number}
+              handleDelete={() => handleDelete(id)}
+            />
           )
         })
       }
