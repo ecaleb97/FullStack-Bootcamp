715bfd68a46addba580a4f80c879681aa45c9adc
ecaleb97 <valley_rob.0c@icloud.com>
2023-11-30 20:16:58 +0100

    Refactor FilterPerson component

--- part2/phonebook/src/components/FilterPerson.jsx
+++ part2/phonebook/src/components/FilterPerson.jsx
@@ -1,13 +1,13 @@
-import { Input } from "./Input";
-
 export function FilterPerson ({ filterValue, handleFilterChange }) {
   return (
     <div className="flex gap-2 items-center">
       <p>Filter shown with</p>
-      <Input
+      <input 
+        type="text" 
         value={filterValue}
         onChange={handleFilterChange}
-        placeholder="filter"
+        className='px-2 py-[1px] border border-gray-400 rounded-md'
+        placeholder="Search..."
       />
     </div>
   )
