56ccac3375573274f96b15877fb61fbb44b87f94
ecaleb97 <valley_rob.0c@icloud.com>
2023-12-01 15:28:38 +0100

    Refactor API endpoints and add delete functionality

--- part3-phonebook-backend/index.js
+++ part3-phonebook-backend/index.js
@@ -5,28 +5,87 @@ app.use(express.json())
 
 app.disable('x-powered-by')
 
+const PORT = process.env.PORT || 3001
+
 let persons = [
   { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456"
   },
   { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
   },
   { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345"
   },
   { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
   }
 ]
 
-console.log(typeof persons)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  const person = persons.find(person => person.id === id)
+
+  if(person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+function generateId() {
+  const ids = persons.map(person => person.id)
+  const maxId = persons.length > 0 
+    ? Math.max(...ids)
+    : 0
+
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+
+  if(!person.name || !person.number) {
+    return response.status(400).json({
+      error: 'name or number is missing'
+    })
+  }
+
+  const newPerson = {
+    id: generateId(),
+    name: person.name,
+    number: person.number
+  }
+
+  persons = [...persons, newPerson]
+
+  console.log(persons)
+
+  response.json(newPerson)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
