d28ce6b07a6f80060fc07f98bc3677acc30f502e
ecaleb97 <valley_rob.0c@icloud.com>
2023-12-02 12:13:00 +0100

    Add CORS middleware to enable cross-origin resource sharing

--- part3-phonebook-backend/index.js
+++ part3-phonebook-backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.disable('x-powered-by')
 
 const PORT = process.env.PORT || 3001
